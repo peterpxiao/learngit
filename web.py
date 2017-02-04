33487d8b4b4df595fb0c9424a85471f3ca37f11d
pp <290093285@qq.com>
2017-02-04 19:37:44 +0800

    add webapp.py

---
+++ web.py
@@ -0,0 +1,13 @@
+for x in range(100000):
+    if x % 1 == 0:
+        if x % 2 == 1:
+            if x % 3 == 0:
+                if x % 4 == 1:
+                    if x % 5 == 1:
+                        if x % 6 == 3:
+                            if x % 7 == 0:
+                                if x % 8 == 1:
+                                    if x % 9 == 0:
+                                        print(x)
+
+
